edc528532e6815368c987e76d6ec0818dfd00cb7
Touracht <tebogomataba6810@gmail.com>
2024-10-10 03:51:05 +0200

    Modify settings_copy.py

--- recipe_api/recipe_api/settings_copy.py
+++ recipe_api/recipe_api/settings_copy.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    'recipe_api.accounts',
     'rest_framework',
     'rest_framework.authtoken',
     'notifications',
