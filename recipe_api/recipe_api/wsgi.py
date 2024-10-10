6321f8b5add67bbdece7ec567bc8c1224cdb910f
Touracht <tebogomataba6810@gmail.com>
2024-10-10 03:41:24 +0200

    Modify wsgi.py

--- recipe_api/recipe_api/wsgi.py
+++ recipe_api/recipe_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipe_api.settings copy')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipe_api.settings_copy')
 
 application = get_wsgi_application()
