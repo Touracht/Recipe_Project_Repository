dce5df6eb087e4ae26cea5206601027606abb923
Touracht <tebogomataba6810@gmail.com>
2024-10-10 15:38:56 +0200

    Modify settings.py

--- recipe_api/recipe_api/settings.py
+++ recipe_api/recipe_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('django-insecure-!l$pjy!=6_pap)*k9^&a1)g81*%5mup_7ko8l_2bx&=i821ugx')
+SECRET_KEY = 'django-insecure-!l$pjy!=6_pap)*k9^&a1)g81*%5mup_7ko8l_2bx&=i821ugx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
